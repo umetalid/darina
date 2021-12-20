1a1526853d1a6dc0446861d5d2c9a54a954881b2
umetalid <umetalid@gmail.com>
2021-12-20 11:48:32 +0600

    pages, colors, strings, id, переход

--- app/src/main/java/com/example/darina/Login.kt
+++ app/src/main/java/com/example/darina/Login.kt
@@ -4,17 +4,17 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.darina.databinding.SigninBinding
+import com.example.darina.databinding.ActivityLoginBinding
+import com.example.darina.databinding.ActivitySigninBinding
 
 class Login : AppCompatActivity() {
-    private lateinit var binding: activity_signin
-    @SuppressLint("setTextI18n")
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= SigninBinding.inflate(layoutInflater)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.signinmain.setOnClickListener {
+        binding.signin1.setOnClickListener {
             startActivity(Intent(this, Signin::class.java))
             finish()
         }
